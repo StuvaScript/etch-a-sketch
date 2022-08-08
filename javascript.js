e3e0dabfe3f9df13f1af567be40f87a6baf00347
Stu <stumac1000@yahoo.com>
2022-08-08 11:45:01 -0700

    Make loop for squares to be shown

--- javascript.js
+++ javascript.js
@@ -1,3 +1,16 @@
-const square = document.createElement('div');
-square.classList.add('square');
-container.appendChild(square);
+// const square = document.createElement('div');
+// square.classList.add('square');
+// container.appendChild(square);
+
+// const square2 = document.createElement('div');
+// square2.classList.add('square');
+// container.appendChild(square2);
+
+let buttz = document.createElement('div');
+for (let i = 1; i <= 5; i++) {
+    buttz[i] = document.createElement('div');
+    buttz[i].classList.add('square');
+    container.appendChild(buttz[i]);
+    console.log(buttz[i] +' inside');
+}
+console.log(buttz + ' outside');
