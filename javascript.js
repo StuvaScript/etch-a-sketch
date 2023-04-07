91e1ded7a5978980af16b3786f1bc5ab254912f9
Stu <stumac1000@yahoo.com>
2023-04-07 12:15:55 -0700

    test save

--- javascript.js
+++ javascript.js
@@ -1,66 +1,65 @@
-
 let choice;
 
 // Starting grid size
 
 baseArea();
 function baseArea() {
-    choice = 16;
+  choice = 16;
 }
 
 // Grid creator
 
 createGrid();
 function createGrid() {
-    if (!(choice > 2 || choice < 100) || (choice < 2 || choice > 100)) return;
-    if (document.querySelector('.square')) {
-        let beGone = document.querySelectorAll('.square');
-        beGone.forEach(beGone => {
-            beGone.remove();
-        });
-    }
-    let grid = choice * choice;
-    document.documentElement.style.setProperty('--grid-size', choice);
-    for (let i = 0; i < grid; i++) {
-        let square = document.createElement('div');
-        square.classList.add('square');
-        container.appendChild(square);
-    }
+  if (!(choice > 2 || choice < 100) || choice < 2 || choice > 100) return;
+  if (document.querySelector(".square")) {
+    let beGone = document.querySelectorAll(".square");
+    beGone.forEach((beGone) => {
+      beGone.remove();
+    });
+  }
+  let grid = choice * choice;
+  document.documentElement.style.setProperty("--grid-size", choice);
+  for (let i = 0; i < grid; i++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+  }
 }
 
 // Hover color changer
 
 colors();
 function colors() {
-    let changeSquare = document.querySelectorAll('.square');
-    changeSquare.forEach(changeSquare => {
-        let lightNumber = 1;
-        
-        changeSquare.addEventListener('mouseover', () => {
-            changeSquare.style.setProperty('background', randomColor());
-            changeSquare.style.setProperty('filter', `brightness(${lightNumber})`);
-            changeLight();
-            function changeLight() {
-                if (lightNumber <= 0) return;
-                lightNumber = (lightNumber - 0.1).toFixed(1);
-            }
-        })
-    })
+  let changeSquare = document.querySelectorAll(".square");
+  changeSquare.forEach((changeSquare) => {
+    let lightNumber = 1;
+
+    changeSquare.addEventListener("mouseover", () => {
+      changeSquare.style.setProperty("background", randomColor());
+      changeSquare.style.setProperty("filter", `brightness(${lightNumber})`);
+      changeLight();
+      function changeLight() {
+        if (lightNumber <= 0) return;
+        lightNumber = (lightNumber - 0.1).toFixed(1);
+      }
+    });
+  });
 }
 
 function randomColor() {
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
-    return `rgb(${r},${g},${b})`;
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  return `rgb(${r},${g},${b})`;
 }
-
+// hey
 // Grid resizing button
 
-let btn = document.querySelector('#btn');
-btn.addEventListener('click', () => {
-    let answer = prompt('Choose a grid size from 2 - 100', )
-    choice = parseInt(answer);
-    createGrid();
-    colors();
-})
+let btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+  let answer = prompt("Choose a grid size from 2 - 100");
+  choice = parseInt(answer);
+  createGrid();
+  colors();
+});
